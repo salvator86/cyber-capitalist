9218d994ec0630f2e48e52a2db0738cd27b1217a
Oleksandr Chukh <demkov.sasha42@gmail.com>
2022-11-15 15:23:25 +0200

    change header position

--- src/app/features/start-game-screen/start-game-screen.component.ts
+++ src/app/features/start-game-screen/start-game-screen.component.ts
@@ -20,4 +20,30 @@ export class StartGameScreenComponent implements OnInit {
     this.businesses = this.pointManager.businesses;
   }
 
+  // mouseDown = false;
+  //
+  // startY: any;
+  //
+  // scrollTop: any;
+  //
+  // slider = document.querySelector<HTMLElement>('.start-game-businesses');
+
+  // startDragging(e: any, flag: any, el: any) {
+  //   this.mouseDown = true;
+  //   this.startY = e.pageY - el.offsetTop;
+  //   this.scrollTop = el.scrollTop;
+  // }
+  // stopDragging(e: any, flag: any) {
+  //   this.mouseDown = false;
+  // }
+  // moveEvent(e: any, el: any) {
+  //   e.preventDefault();
+  //   if (!this.mouseDown) {
+  //     return;
+  //   }
+  //   const y = e.pageY - el.offsetTop;
+  //   const scroll = y - this.startY;
+  //   el.scrollTop = this.scrollTop - scroll;
+  // }
+
 }
